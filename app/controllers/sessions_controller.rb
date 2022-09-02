f8bb81ea8831c5c3a36dfce4729961a528eadaee
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-09-01 17:12:34 -0700

    Completed Assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete :user_id
-    head :no_content
+    # head :no_content
+    render json: {}, status: :no_content
   end
 
 end
